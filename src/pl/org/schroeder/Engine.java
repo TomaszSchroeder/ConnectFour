ea47a1298999a4dffca9c582d7de8b7404fe0218
Tomasz <tomasz@schroeder.city>
2019-04-30 19:26:47 +0200

    some changes

--- src/pl/org/schroeder/Engine.java
+++ src/pl/org/schroeder/Engine.java
@@ -1,14 +1,19 @@
 package pl.org.schroeder;
 
+import javafx.application.Platform;
 import javafx.scene.layout.Pane;
 
 public class Engine {
 
     Layout.BuildLayout buildLayout;
+    Board board;
+    Layout layout;
+    Engine engine;
 
     public Engine() {
 
     }
+
 
     public void buttonHandling(Board board, Pane discField) {
         placeRedChecker(board, discField);
@@ -22,6 +27,8 @@ public class Engine {
         placeYellowChecker(board, discField);
 
         checkYellowWinner(board);
+
+        Platform.runLater(() -> afterTurns(board));
     }
 
     public void checkRedWinner(Board board) {
@@ -65,7 +72,7 @@ public class Engine {
         int xAxisRed = -640 + ((xPosRed - 1) * 95);
         int yAxisRed = 540 - ((yPosRed - 1) * 95);
 
-        discField.getChildren().add(buildLayout.drawRedDisc(xAxisRed, yAxisRed, 45));
+        discField.getChildren().add(Layout.BuildLayout.drawRedDisc(xAxisRed, yAxisRed, 45));
     }
 
     public void placeYellowChecker(Board board, Pane discField) {
@@ -75,8 +82,18 @@ public class Engine {
         int xAxisYel = -640 + ((xPosYel - 1) * 95);
         int yAxisYel = 540 - ((yPosYel - 1) * 95);
 
-        discField.getChildren().add(buildLayout.drawYellowDisc(xAxisYel, yAxisYel, 45));
+        discField.getChildren().add(Layout.BuildLayout.drawYellowDisc(xAxisYel, yAxisYel, 45));
     }
 
-
+    public void afterTurns(Board board) {
+        if ((board.checkWinningCondition("Red")) || (board.checkWinningCondition2("Red")) || (board.checkWinningCondition3("Red"))) {
+            System.out.println("Red Wins");
+            wait3sec();
+            buildLayout.displayWin();
+        } else if ((board.checkWinningCondition("Yellow")) || (board.checkWinningCondition2("Yellow")) || (board.checkWinningCondition3("Yellow"))) {
+            System.out.println("Yellow Wins");
+            wait3sec();
+            buildLayout.displayLose();
+        }
+    }
 }
--- src/pl/org/schroeder/Main.java
+++ src/pl/org/schroeder/Main.java
@@ -1,8 +1,9 @@
 package pl.org.schroeder;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 
@@ -14,11 +15,30 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         //stage = primaryStage;
+//        Engine engine = new Engine();
 
         Layout.BuildLayout buildLayout = new Layout.BuildLayout();
         buildLayout.displayStart();
-    }
 
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                Runnable inGame = new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        engine.afterTurns();
+//                    }
+//                };
+//
+//
+//                    Platform.runLater(inGame);
+//
+//            }
+//        });
+//
+//        thread.setDaemon(true);
+//        thread.start();
+    }
 }
 
 
